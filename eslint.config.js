3f1ed1edbaf4b31e69e4506511ed8310fc295336
Abdon Morales <abdonmorales@utexas.edu>
2024-06-01 05:55:47 +0000

    Created file to transition linter from the old format as of v9.0 of eslint

---
+++ eslint.config.js
@@ -0,0 +1,14 @@
+import parser from '@typescript-eslint/parser';
+import tp = '@typescrtipt-eslint';
+
+export default [
+    {
+        languageOptions: {
+            ecmaVersion: 2020,
+            sourceType: 'module',
+        },
+        rules: {
+            
+        }
+    }
+]
