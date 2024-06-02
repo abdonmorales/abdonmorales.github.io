1f8db4fd9172b81cf97164d5241ebf88a3983962
Abdon Morales <abdonmorales@utexas.edu>
2024-06-02 14:25:24 +0000

    Adding the new lint configuration file (migration completed)

--- eslint.config.js
+++ eslint.config.js
@@ -1,14 +1,14 @@
-import parser from '@typescript-eslint/parser';
-import tp = '@typescrtipt-eslint';
-
-export default [
-    {
-        languageOptions: {
-            ecmaVersion: 2020,
-            sourceType: 'module',
-        },
-        rules: {
-            
-        }
+export default {
+    parser: "@typescript-eslint/parser",
+    extends: [
+        "plugin:@typescript-eslint/recommended"
+    ],
+    parserOptions: {
+        ecmaVersion: 2020,
+        sourceType: "module"
+    },
+    rules: {
+        "@typescript-eslint/explicit-function-return-type": "off",
+        "@typescript-eslint/triple-slash-reference": "off"
     }
-]
+};
